89ff67e6a696574e50cea35b8c8da61025e7cc1d
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-11 09:17:26 +0700

    feat(application): implement product management service logic and mapping

---
+++ Ecommerce/Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,14 @@
+using Application.Common.Response;
+using Application.DTOs.Product;
+
+namespace Application.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<Result<ProductResponse>> GetProductByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Result<List<ProductResponse>>> GetAllProductsAsync(CancellationToken cancellationToken = default);
+        Task<Result<ProductResponse>> CreateProductAsync(CreateProductRequest createProductRequest, CancellationToken cancellationToken = default);
+        Task<Result<ProductResponse>> UpdateProductAsync(Guid id, UpdateProductRequest updateProductRequest, CancellationToken cancellationToken = default);
+        Task<Result<bool>> DeleteProductAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
