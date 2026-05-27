1c5aca9e2d3cf0f8b3ba1643b7bc5afa22c64de4
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-27 08:56:03 +0700

    refactor(dto): strengthen validations for all input request DTOs

--- Ecommerce/Application/DTOs/Checkout/CheckoutRequest.cs
+++ Ecommerce/Application/DTOs/Checkout/CheckoutRequest.cs
@@ -1,7 +1,10 @@
 using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.DTOs.Checkout
 {
     public sealed record CheckoutRequest(
+        [Required(ErrorMessage = "PaymentMethod is required.")]
+        [EnumDataType(typeof(PaymentMethod), ErrorMessage = "Invalid PaymentMethod.")]
         PaymentMethod PaymentMethod);
 }
