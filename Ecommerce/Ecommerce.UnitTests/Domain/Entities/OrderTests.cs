8ec6dbd90fe019cfe1170d9f85ce30036a3f4311
DUYOCSCHOS <duy1632005@gmail.com>
2026-06-02 15:29:03 +0700

    test(order event): test order completed event credit loyalty point properly

--- Ecommerce/Ecommerce.UnitTests/Domain/Entities/OrderTests.cs
+++ Ecommerce/Ecommerce.UnitTests/Domain/Entities/OrderTests.cs
@@ -65,6 +65,72 @@ namespace Ecommerce.UnitTests.EntityTests
                 .WithMessage("Cannot mark an order in 'Returned' status as delivered.");
         }
 
+        [Fact]
+        public void MarkAsCompleted_WhenOrderCanBeCompleted_SetsStatusToCompleted()
+        {
+            var order = CreateOrder(OrderStatus.Delivered);
+
+            order.MarkAsCompleted();
+
+            order.Status.Should().Be(OrderStatus.Completed);
+        }
+
+        [Fact]
+        public void MarkAsCompleted_WhenOrderCanBeCompleted_RaisesOrderCompletedDomainEvent()
+        {
+            var order = CreateOrder(OrderStatus.Delivered);
+
+            order.MarkAsCompleted();
+
+            order.DomainEvents.Should().ContainSingle(e => e is OrderCompletedDomainEvent);
+        }
+
+        [Fact]
+        public void MarkAsCompleted_WhenAlreadyCompleted_DoesNotRaiseDuplicateEvent()
+        {
+            var order = CreateOrder(OrderStatus.Delivered);
+
+            order.MarkAsCompleted();
+            order.ClearDomainEvents();
+            order.MarkAsCompleted();
+
+            order.Status.Should().Be(OrderStatus.Completed);
+            order.DomainEvents.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void MarkAsCompleted_WhenCancelled_ThrowsInvalidOperationException()
+        {
+            var order = CreateOrder(OrderStatus.Cancelled);
+
+            Action act = () => order.MarkAsCompleted();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("Cannot mark an order in 'Cancelled' status as completed.");
+        }
+
+        [Fact]
+        public void MarkAsCompleted_WhenReturned_ThrowsInvalidOperationException()
+        {
+            var order = CreateOrder(OrderStatus.Returned);
+
+            Action act = () => order.MarkAsCompleted();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("Cannot mark an order in 'Returned' status as completed.");
+        }
+
+        [Fact]
+        public void MarkAsCompleted_WhenNotDelivered_ThrowsInvalidOperationException()
+        {
+            var order = CreateOrder(OrderStatus.Confirmed);
+
+            Action act = () => order.MarkAsCompleted();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("Cannot mark an order in 'Confirmed' status as completed.");
+        }
+
         private static Order CreateOrder(OrderStatus status)
         {
             var order = Order.Create(Guid.NewGuid(), 100001, PaymentMethod.COD);
