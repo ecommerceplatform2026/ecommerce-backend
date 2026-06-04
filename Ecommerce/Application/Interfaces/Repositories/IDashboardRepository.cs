8edc3f708bb60ab218f3bffb778e19c89b0db518
Khánh Hưng <khanhhungzm@gmail.com>
2026-06-04 15:17:50 +0700

    feat(dashboard): add dashboard service and repository interfaces

--- Ecommerce/Application/Interfaces/Repositories/IDashboardRepository.cs
+++ Ecommerce/Application/Interfaces/Repositories/IDashboardRepository.cs
@@ -1,4 +1,5 @@
 using Application.DTOs.Dashboard;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,5 +8,7 @@ namespace Application.Interfaces.Repositories
     public interface IDashboardRepository
     {
         Task<DashboardSummaryResponse> GetDashboardSummaryAsync(DashboardRequest request, CancellationToken cancellationToken = default);
+        Task<List<RevenueTrendResponse>> GetRevenueTrendAsync(DashboardRequest request, CancellationToken cancellationToken = default);
+        Task<List<PaymentMethodSummaryResponse>> GetPaymentMethodSummaryAsync(DashboardRequest request, CancellationToken cancellationToken = default);
     }
 }
